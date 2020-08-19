e997267d894c3a51d057a88bde6838955350a19a
dindean <a123deandean@gmail.com>
2020-08-19 15:36:12 -0400

    8/19 3:30pm

--- client/components/Navbar.jsx
+++ client/components/Navbar.jsx
@@ -1,16 +1,50 @@
 import { Navbar, Nav, Button } from 'react-bootstrap';
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faGoogle } from '@fortawesome/free-brands-svg-icons'
-import { faFeatherAlt } from '@fortawesome/free-solid-svg-icons'
+import React, { useState, useEffect } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGoogle } from '@fortawesome/free-brands-svg-icons';
+import { faFeatherAlt } from '@fortawesome/free-solid-svg-icons';
+import SignDropdown from './small-components/SignDropdown.jsx';
+import { set } from 'js-cookie';
 
-export default function Notnav() {
+export default function Notnav({ handleStatusChange, userName, handleLogOut }) {
+  const [hidden, setHidden] = useState(true);
+
+  function handleClick() {
+    setHidden(!hidden);
+    console.log(hidden);
+  }
+
   return (
-  <Navbar expand="lg" className="myNavbar justify-content-between">
-    <Navbar.Brand className="brand" href="#home"><FontAwesomeIcon icon={faFeatherAlt} /> Social Scrapbook </Navbar.Brand>
-    <Nav>
-        <a href="/api/login"><Button className="navButton" variant="outline-primary"><FontAwesomeIcon icon={faGoogle} /> Sign Up/Log In</Button></a>
-    </Nav>
-  </Navbar>
-  )
+    <Navbar expand="lg" className="myNavbar justify-content-between">
+      <Navbar.Brand className="brand" href="#home">
+        <FontAwesomeIcon icon={faFeatherAlt} /> Social Scrapbook{' '}
+      </Navbar.Brand>
+      <Nav>
+        <a href="#">
+          <Button
+            className="navButton"
+            variant="outline-primary"
+            onClick={userName ? handleLogOut : handleClick}
+          >
+            {userName ? 'Log Out' : 'Sign Up / Log In'}
+          </Button>
+        </a>
+      </Nav>
+      {hidden ? null : (
+        <SignDropdown
+          handleStatusChange={handleStatusChange}
+          hideAfterSignInOrUp={() => {
+            setHidden(true);
+          }}
+        />
+      )}
+      <Nav>
+        <a href="/api/login">
+          <Button className="navButton" variant="outline-primary">
+            <FontAwesomeIcon icon={faGoogle} /> Sign Up/Log In With Google
+          </Button>
+        </a>
+      </Nav>
+    </Navbar>
+  );
 }
--- server/server.js
+++ server/server.js
@@ -4,11 +4,14 @@ const cookieParser = require('cookie-parser');
 
 const app = express();
 const apiRouter = require('./routers/api');
+const dotenv = require('dotenv').config();
+const cors = require('cors');
 
 // BODY PARSERS & COOKIE PARSER
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(cookieParser());
+app.use(cors());
 
 // SERVE UP STATIC FILES
 app.use('/dist', express.static(path.join(__dirname, '../dist')));
@@ -31,6 +34,6 @@ app.use((err, req, res, next) => {
   res.status(401).send(err.message); // WHAT IS FRONT-END EXPECTING? JSON OR STRING?
 });
 
-//app.listen(3000); //listens on port 3000 -> http://localhost:3000/
+// app.listen(3000); //listens on port 3000 -> http://localhost:3000/
 app.listen(process.env.PORT || 3000);
 module.exports = app;
